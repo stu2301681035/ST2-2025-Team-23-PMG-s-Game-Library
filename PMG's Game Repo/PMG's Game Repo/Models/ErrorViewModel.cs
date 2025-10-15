37c80572dc9e8473d66081f69521ef64c6a5d168
stu2301681035 <stu2301681035@uni-plovdiv.bg>
2025-10-15 16:23:20 +0300

    Add files via upload

---
+++ PMG's Game Repo/PMG's Game Repo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PMG_s_Game_Repo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
