b8c9aa072f97e08ff68046ea3f09421b55efa14e
George <stu2301681026@uni-plovdiv.bg>
2025-10-16 02:38:37 +0300

    Guys, hope I ain't waking anybody up, best regards yours truly George, neighbourhood's computer masochist :)

---
+++ PMG's Game Repo/PMG's Game Repo/DTOs/RawgGameDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class RawgGameDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Released { get; set; }
+    public double Rating { get; set; }
+    public string Background_Image { get; set; }
+    public List<RawgGenreDto> Genres { get; set; }
+    public List<RawgPlatformDto> Platforms { get; set; }
+}
